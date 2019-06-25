e3df098e73c39e1efe1cb5826b052576bcd36bbb
pengyou <120465745@qq.com>
2019-06-25 17:20:35 +0800

    修改

--- springboot02/server/src/main/java/com/pengyou/controller/ExcelController.java
+++ springboot02/server/src/main/java/com/pengyou/controller/ExcelController.java
@@ -62,6 +62,12 @@ public class ExcelController {
         return response;
     }
 
+    /**
+     * 不分sheet,poi导出我们的商品表格
+     * @param name
+     * @param response
+     * @return
+     */
     @RequestMapping(value = prefix+"/product/export",method = RequestMethod.GET)
     //这个@ResponseBody 作用是以流的形式返回给浏览器
     public @ResponseBody String export(String name, HttpServletResponse response){
@@ -83,7 +89,37 @@ public class ExcelController {
         }catch (Exception e){
             e.printStackTrace();
         }
+        return null;
+    }
 
+
+    /**
+     * 分sheet,poi导出我们的商品表格
+     * @param name
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = prefix+"/product/exportv2",method = RequestMethod.GET)
+    //这个@ResponseBody 作用是以流的形式返回给浏览器
+    public @ResponseBody String exportv2(String name, HttpServletResponse response){
+        //定义表格头信息
+        String[] headers=new String[]{"名称","单位","单价","库存量","备注","采购日期"};
+        //根据名字查询所有产品信息
+        List<Product> products=productMapper.selectAll(name);
+        try {
+
+            if(products!=null && products.size()>0) {
+                //TODO：将产品信息列表封装在map中list->list-map
+                List<Map<Integer, Object>> listMap = productService.manageProductList(products);
+                //TODO：v2 分sheet导出实战,将list-map塞入真正的excel对应的workbook(相当于excel)并且返回workbook
+                Workbook wb = poiService.manageSheet(listMap, headers, env.getProperty("poi.product.excel.sheet.name"));
+                //TODO：利用我们之前的通用下载附件工具,将excel实例(workbook)以流的形式写回浏览器
+                webOperationService.downloadExcel(response, wb, env.getProperty("poi.product.excel.file.name"));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
