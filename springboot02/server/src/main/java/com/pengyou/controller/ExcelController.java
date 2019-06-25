ea540148348928e960d51b2b1b7e2ce7f4245da8
pengyou <120465745@qq.com>
2019-06-25 15:51:42 +0800

    增加全局异常处理

---
+++ springboot02/server/src/main/java/com/pengyou/controller/ExcelController.java
@@ -0,0 +1,91 @@
+package com.pengyou.controller;
+
+import com.pengyou.enums.StatusCode;
+import com.pengyou.model.entity.Product;
+import com.pengyou.model.mapper.ProductMapper;
+import com.pengyou.response.BaseResponse;
+import com.pengyou.service.PoiService;
+import com.pengyou.service.ProductService;
+import com.pengyou.service.WebOperationService;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class ExcelController {
+
+    private static final Logger log= LoggerFactory.getLogger(ExcelController.class);
+
+    private static final String prefix="excel";
+
+    @Autowired(required = false)
+    private ProductMapper productMapper;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private PoiService poiService;
+
+    @Autowired
+    private WebOperationService webOperationService;
+
+
+    /**
+     * 查询产品信息列表
+     * @param name
+     * @return
+     */
+    @RequestMapping(value = prefix+"/product/list",method = RequestMethod.GET)
+    @ResponseBody
+    public BaseResponse list(String name){
+        BaseResponse response=new BaseResponse(StatusCode.Success);
+        try {
+            List<Product> products=productMapper.selectAll(name);
+            response.setData(products);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    @RequestMapping(value = prefix+"/product/export",method = RequestMethod.GET)
+    //这个@ResponseBody 作用是以流的形式返回给浏览器
+    public @ResponseBody String export(String name, HttpServletResponse response){
+        //定义表格头信息
+        String[] headers=new String[]{"名称","单位","单价","库存量","备注","采购日期"};
+        //根据名字查询所有产品信息
+        List<Product> products=productMapper.selectAll(name);
+        try {
+
+            if(products!=null && products.size()>0) {
+                //TODO：将产品信息列表封装在map中list->list-map
+                List<Map<Integer, Object>> listMap = productService.manageProductList(products);
+                //TODO：将list-map塞入真正的excel对应的workbook(相当于excel)并且返回workbook
+                Workbook wb = poiService.fillExcelSheetData(listMap, headers, env.getProperty("poi.product.excel.sheet.name"));
+                //TODO：利用我们之前的通用下载附件工具,将excel实例(workbook)以流的形式写回浏览器
+                webOperationService.downloadExcel(response, wb, env.getProperty("poi.product.excel.file.name"));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+}
